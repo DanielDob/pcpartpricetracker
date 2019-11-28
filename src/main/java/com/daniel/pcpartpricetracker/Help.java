627ba8bb874e386f7852156d1202a0f676baeb9e
DanielDob <35843305+DanielDob@users.noreply.github.com>
2019-11-29 00:13:01 +0100

    Update Help.java
    reorganiation

--- src/main/java/com/daniel/pcpartpricetracker/Help.java
+++ src/main/java/com/daniel/pcpartpricetracker/Help.java
@@ -3,6 +3,47 @@ package com.daniel.pcpartpricetracker;
 public class Help {
 	
 	public static void printFullHelp(){
+		printBuildFullHelp(false);
+		printPartFullHelp(false);
+		printScrapFullHelp(false);
+		System.out.println();
+		printTerminologyUsedInFullHelp();
+		
+			
+	}
+	private static void printTerminologyUsedInFullHelp() {
+		System.out.println(	"date - dd.MM.yyyy\n"
+				+ "number - number in link in ceneo"
+				+ "type - cpu|gpu|ps|mobo|hdd|sdd|cooler|case|fan|ram|m2|psu");
+		
+	}
+	private static void printScrapFullHelp(boolean b) {
+		System.out.println( "scrap\n"
+				+ "\tGo scrap");
+		/*
+		 * if(b)
+		 * 	prints terminology used
+		 */
+		
+	}
+	private static void printPartFullHelp(boolean b) {
+		System.out.println(
+				 "part\n"
+					+ "\tadd [number] [type]\n"
+					+ "\t\tAdding new part to tracking price\\n"
+					+ "\topen [number]|\n"
+					+ "\t\tOpens in broser part\n"
+					+ "\tshow [[number] | [-all]] [-d] [-h|-h [date][date]] [-sn]\n"
+					+ "\t\tDispalying part information by default shows only full name. -d details. -h history. -sn short name."
+					+ "\txlsx ['name'] [-a] [-d [date]]\n"
+					+ "\t\tSave history to xlsx file. -a all. -d date.\n"
+					);
+		if(b)
+			printTerminologyUsedInFullHelp();
+			
+		
+	}
+	public static void printBuildFullHelp(boolean b) {
 		System.out.println(
 				"build\n"
 				+ "\tadd ['name'] [number, number,...]"
@@ -15,21 +56,8 @@ public class Help {
 				+ "\t\tRemoves parts"
 				+ "\txlsx ['name'] [-a] [-d [date]]"
 				+ "\t\tSave history to xlsx file. -a all. -d date. "
-			+ "part\n"
-				+ "\tadd [number] [type]\n"
-				+ "\t\tAdding new part to tracking price\\n"
-				+ "\topen [number]|\n"
-				+ "\t\tOpens in broser part\n"
-				+ "\tshow [[number] | [-all]] [-d] [-h|-h [date][date]] [-sn]\n"
-				+ "\t\tDispalying part information by default shows only full name. -d details. -h history. -sn short name."
-				+ "\txlsx ['name'] [-a] [-d [date]]\n"
-				+ "\t\tSave history to xlsx file. -a all. -d date.\n"
-			+ "scrap\n"
-				+ "\tGo scrap"
-				+ "\n\n"
-				+ "date - dd.MM.yyyy\n"
-				+ "number - number in link in ceneo"
-				+ "type - cpu|gpu|ps|mobo|hdd|sdd|cooler|case|fan|ram|m2|psu"
 				);
+		if(b)
+			printTerminologyUsedInFullHelp();
 	}
 }
